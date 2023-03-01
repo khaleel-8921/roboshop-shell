aff7281e800ba6c7ce980de4ad4668250f193ae4
Khaleel <pathankhaleel8@gmail.com>
2023-03-01 23:09:44 +0530

    roboshop components

--- mongodb.sh
+++ mongodb.sh
@@ -1,8 +1,7 @@
-cp configs/
+cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 
 
 yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
-127.0.0.1 to 0.0.0.0
-systemctl restart mongod
+# update /etc/mongod.conf file from 127.0.0.1 with  0.0.0.0
