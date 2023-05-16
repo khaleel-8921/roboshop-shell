0cf5a1f1ce00ea570fe17a63dde03600eb89fef3
Khaleel <pathankhaleel8@gmail.com>
2023-05-17 00:05:44 +0530

    updating roboshop-shell

--- mongodb.sh
+++ mongodb.sh
@@ -1,11 +1,4 @@
-code_dir=$(pwd)
-log_file=/tmp/roboshop.log
-rm -f ${log_file}
-
-print_head(){
-  echo -e "\e[31m$1\e[0m"
-}
-
+common.sh
 print_head "setup mongod repository"
 cp configs/mongodb.repo /etc/yum.repos.d/mongo.repo
 
