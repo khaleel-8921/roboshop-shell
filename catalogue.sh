9f4f9be3050c3700748613b1b1220074cab88517
Khaleel <pathankhaleel8@gmail.com>
2023-05-17 11:31:56 +0530

    updating roboshop-shell

--- catalogue.sh
+++ catalogue.sh
@@ -18,7 +18,7 @@ rm -rf /app/* &>>${log_file}
 
 print_head "Download App content"
 curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>${log_file}
-cd /app
+
 
 print_head "Extracting the App content"
 unzip /tmp/catalogue.zip &>>${log_file}
