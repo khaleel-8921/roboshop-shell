1526f1ffe4e841729d9774754b0794ebe0354f87
Khaleel <pathankhaleel8@gmail.com>
2023-05-18 22:17:21 +0530

    updating roboshop-shell

--- common.sh
+++ common.sh
@@ -5,3 +5,14 @@ rm -f ${log_file}
 print_head(){
   echo -e "\e[33m$1\e[0m"
 }
+
+status_check(){
+
+  if [ $1 -eq 0 ]; then
+
+    echo success
+    else
+    echo failure
+  fi
+ }
+
--- frontend.sh
+++ frontend.sh
@@ -1,30 +1,30 @@
 source common.sh
 print_head "Installing nginx"
 yum install nginx -y &>>${log_file}
-echo $?
+status_check $?
 
 print_head "Removing  old content"
 rm -rf /usr/share/nginx/html/* &>>${log_file}
-echo $?
+status_check $?
 
 print_head "Downloading frontend content"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log_file}
-echo $?
+status_check $?
 
 print_head "Extracting the downloaded content"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip &>>${log_file}
-echo $?
+status_check $?
 
 print_head "copying nginx config for roboshop"
 cp ${code_dir}/configs/nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log_file}
-echo $?
+status_check $?
 
 print_head "Enabling nginx"
 systemctl enable nginx &>>${log_file}
-echo $?
+status_check $?
 
 print_head "starting the nginx"
 systemctl restart nginx &>>${log_file}
-echo $?
+status_check $?
 
