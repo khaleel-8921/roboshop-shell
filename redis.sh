f4480f651571a8e5bffba81ced786728654de5c0
Khaleel <pathankhaleel8@gmail.com>
2023-05-29 02:02:13 +0530

    updating roboshop-shell

--- redis.sh
+++ redis.sh
@@ -17,9 +17,9 @@ sed -e -i 's/127.0.0.0.1/0.0.0.0' /etc/redis.conf /etc/redis/redis.conf &>>${log
 status_check $?
 
 print_head "enable redis"
-systemctl enable redis
+systemctl enable redis &>>${log_file}
 status_check $?
 
 print_head "start"
-systemctl start redis
+systemctl start redis &>>${log_file}
 status_check $?
