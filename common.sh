ca3d7ad8e0409a4989c7254a39a2000aaf0dac9d
Khaleel <pathankhaleel8@gmail.com>
2023-06-05 01:08:28 +0530

    updating roboshop-shell

--- common.sh
+++ common.sh
@@ -122,11 +122,8 @@ java(){
   mvn clean package &>>${log_file}
   mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
   status_check $?
-
   #schema setup Function
   schema_setup
-
  # systemD setup Function
   systemD_setup
-
 }
--- shipping.sh
+++ shipping.sh
@@ -1,7 +1,7 @@
 source common.sh
-mysql_root_pasword=$1
-if [ "${mysql_root_password}" == "mysql" ]; then
-  echo -e "\e[31m Missing Mysql Root password argument\e[0m"
+mysql_root_password=$1
+if [ -z "${mysql_root_password}" ]; then
+  echo -e "\e[31m Missing mysql_root_password argument\e[0m"
   exit 1
 fi
 
